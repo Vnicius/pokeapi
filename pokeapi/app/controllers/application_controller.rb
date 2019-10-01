040e905ae5fbc4682a7b78a29fd7fc7f83e8da19
Vinícius <vinicius.matheus252@gmail.com>
2019-10-01 16:42:09 -0300

    Add aplication controller

--- pokeapi/app/controllers/application_controller.rb
+++ pokeapi/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+    before_action :auth_request
+
+    private
+
+    def auth_request
+        @current_user ||= AuthRequest.call(request.headers).result
+        render json: { status: "Unauthorized" }, status: 401 unless @current_user
+    end
 end
