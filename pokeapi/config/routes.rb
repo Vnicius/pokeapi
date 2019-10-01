80dba41a644848e2eb47b1ab7df770f246290091
Vinícius <vinicius.matheus252@gmail.com>
2019-10-01 16:43:33 -0300

    Add new routes

--- pokeapi/config/routes.rb
+++ pokeapi/config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
       get 'pk', to: 'pokemons#show'
       get 'pk/pokedex/:id', to: 'pokemons#get'
       get 'pk/search', to: 'pokemons#search'
+
+      post 'user', to: 'user#create'
+      get 'user/me', to: 'user#me'
+
+      post 'auth/user', to: 'authentication#authenticate'
     end
   end
 
