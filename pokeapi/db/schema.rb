892b65edce76a28e6662989104533384663e25e4
Vinícius <vinicius.matheus252@gmail.com>
2019-10-01 16:43:27 -0300

    Update db schema

--- pokeapi/db/schema.rb
+++ pokeapi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_014849) do
+ActiveRecord::Schema.define(version: 2019_09_28_233600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2019_09_27_014849) do
     t.text "icon"
     t.string "type_1"
     t.string "type_2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
