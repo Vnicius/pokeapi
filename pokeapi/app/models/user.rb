b2eeacf538dc0e22618901100cba58722f73af3c
Vinícius <vinicius.matheus252@gmail.com>
2019-10-01 16:42:39 -0300

    Add User model

--- pokeapi/app/models/user.rb
+++ pokeapi/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /.+@.+\..+/ }
+    validates :password, presence: true, length: { in: 8..15 }
 end
